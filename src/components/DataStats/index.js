76eac78ecb245221289648fa704e9b4bb8b68d6a
flytreeleft <flytreeleft@crazydan.org>
2024-09-13 16:51:37 +0800

    Components: 调整组件 DataStats 的实现机制以支持 SSR 渲染统计结果

--- src/components/DataStats/index.js
+++ src/components/DataStats/index.js
@@ -1,29 +1,30 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React from 'react';
 
-const Context = React.createContext();
+// Note: 在 SSR 模式中，不能通过 React.createContext 在组件间共享数据，
+// 因此，只能递归遍历所有子节点
 export function DataStats({ children }) {
-  const [dataItems, updateDataItems] = useState({});
+  const data = {};
+  statsItems(children, data);
 
-  return (
-    <Context.Provider value={{ dataItems, updateDataItems }}>
-      {children}
-    </Context.Provider>
-  );
+  return replaceChild(children, (child) => {
+    const props = child.props || {};
+    const type = props.originalType;
+
+    if (type === DataSum) {
+      const unit = props.unit;
+      const value = props.value;
+      const sum = (data[unit] || []).reduce((result, curr) => result + curr, 0);
+
+      if (sum !== value) {
+        return DataSum({ unit, value: sum });
+      }
+    }
+
+    return child;
+  });
 }
 
 export function DataItem({ unit, value }) {
-  const { dataItems, updateDataItems } = useContext(Context);
-
-  // 在组件挂载后更新共享数据
-  useEffect(() => {
-    updateDataItems(() => {
-      dataItems[unit] = dataItems[unit] || [];
-      dataItems[unit].push(value);
-
-      return dataItems;
-    });
-  }, []);
-
   return (
     <>
       {value} {unit}
@@ -31,16 +32,51 @@ export function DataItem({ unit, value }) {
   );
 }
 
-export function DataSum({ unit }) {
-  const { dataItems } = useContext(Context);
-  const sum = (dataItems[unit] || []).reduce(
-    (result, curr) => result + curr,
-    0
-  );
-
+export function DataSum({ unit, value = 0 }) {
   return (
     <>
-      {sum} {unit}
+      {value} {unit}
     </>
   );
+}
+
+function statsItems(children, data) {
+  const items = Array.isArray(children) ? children : [children];
+
+  items.forEach((item) => {
+    const props = item.props || {};
+    const type = props.originalType;
+
+    if (type === DataItem) {
+      const unit = props.unit;
+      const value = props.value;
+
+      data[unit] = data[unit] || [];
+      data[unit].push(value);
+    } else {
+      statsItems(props.children || [], data);
+    }
+  });
+}
+
+function replaceChild(children, replace) {
+  if (Array.isArray(children)) {
+    return children.map((child) => {
+      child = replace(child);
+
+      if (child.props && child.props.children) {
+        child = {
+          ...child,
+          props: {
+            ...child.props,
+            children: replaceChild(child.props.children, replace)
+          }
+        };
+      }
+
+      return child;
+    });
+  } else {
+    return replace(children);
+  }
 }
